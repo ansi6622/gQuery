9d4ea9a57f2aab37b2eb51ae3dc00a90f3b1f034
Tim Garcia <tim.m.garcia@gmail.com>
2015-05-21 12:51:08 -0700

    Adding more docs for gQuery

--- gQuery.js
+++ gQuery.js
@@ -2,7 +2,18 @@
 
   // The gQuery function that creates gQuery objects.  The value
   // returned should be a gQuery object.
-  var gQuery = function(selector) {
+  // The qQuery function should be able to handle 3 different types of
+  // of values being passed at the param:
+  // 1. A string which defines the selector to execute on the dom
+  //    and returns dom elements that will be saved as a gQuery obj.
+  // 2. An instance of HTMLElement.  Since the object has already been
+  //    found, no query selector is needed, just return a gQuery object
+  //    with the element saved in an array.
+  // 3. Something that looks like an array (It must have a lenght and
+  //    the length is greater than 0).  The the param looks like an array,
+  //    assume that you are being passed an array of HTMLElements.  Simply
+  //    add that array as a parameter to the gQuery object.
+  var gQuery = function(param) {
   }
 
   // A gQuery object will keep track of the selector used to find the
@@ -14,16 +25,20 @@
   // Otherwise, sets the value of the specified property.
   // The get computed style method might be useful here:
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  // The css method should support chainging.  In other words,
+  // the gQuery object should be returned when a property is set.
   gQueryObj.prototype.css = function(property, value) {
   };
 
   // Gets the html of the first element in the list of elements
   // if not htmlString is specified.  If an html string is specified,
   // set the html to each elements inner html.
+  // Also support chaining.
   gQuery.prototype.html = function(htmlString) {
   };
 
-  //Implement the attr() method.
+  // TODO: BONUS!!
+  //BONUS: Implement the attr() method.
 
   window.gQuery = gQuery;
   window.$ = window.gQuery;
